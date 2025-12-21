bd3ee974715c365e33993a81ddec8872bd37a7e2
bubliks7 <bubliksiur@gmail.com>
2025-12-21 18:05:46 +0100

    register works fine

--- users/forms.py
+++ users/forms.py
@@ -1,35 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class RejestracjaForm(forms.ModelForm):
+class RejestracjaForm(forms.Form):
     login = forms.CharField(max_length=150, label='Nazwa użytkownika')
     haslo = forms.CharField(widget=forms.PasswordInput, label='Hasło')
     potwierdz_haslo = forms.CharField(widget=forms.PasswordInput, label="Potwierdź hasło")
     imie = forms.CharField(max_length=75, label='Imię')
     nazwisko = forms.CharField(max_length=80, label='Nazwisko')
     email = forms.EmailField(label='Email')
-
-    class Meta:
-        model = User
-        fields = ['login', 'haslo', 'potwierdz_haslo', 'imie', 'nazwisko', 'email']
     
     def clean(self):
-        wyczyszczone = super().clean()
-        haslo = wyczyszczone.get('haslo')
-        potwierdz_haslo = wyczyszczone.get('potwierdz_haslo')
+        cleaned_data = super().clean()
+        haslo = cleaned_data.get('haslo')
+        potwierdz_haslo = cleaned_data.get('potwierdz_haslo')
 
-        if haslo and potwierdz_haslo and haslo != potwierdz_haslo:
+        if haslo != potwierdz_haslo:
             raise forms.ValidationError('Hasla musza byc takie same')
-        return wyczyszczone
+        return cleaned_data
 
-    def save(self, commit=True):
-        uzytkownik = User.objects.create_user(
-            username=self.wyczyszczone['login'],
-            password=self.wyczyszczone['haslo'],
-            first_name=self.wyczyszczone['imie'],
-            last_name=self.wyczyszczone['nazwisko'],
-            email=self.wyczyszczone['email']
+    def save(self):
+        return User.objects.create_user(
+            username=self.cleaned_data['login'],
+            password=self.cleaned_data['haslo'],
+            first_name=self.cleaned_data['imie'],
+            last_name=self.cleaned_data['nazwisko'],
+            email=self.cleaned_data['email']
         )
-        if commit:
-            uzytkownik.save()
-        return uzytkownik
